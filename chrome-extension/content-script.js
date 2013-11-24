0f1d393c516744e11ab61b51d4e75ef9a2439b53
Calum J. Eadie <calum@calumjeadie.com>
2013-11-24 17:38:28 +0000

    Outlined chrome extension

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,1 +1,49 @@
-console.log("console-script.js started")
+console.log("console-script.js started");
+
+ug = {};
+
+ug.FILE_VIEW = "#files .file .blob-wrapper"
+ug.UXF_LINE = "#files .line";
+
+ug.getUXF = function() {
+    lines = $(ug.UXF_LINE).map(function(i, el) {
+        return $(el).text();
+    });
+    uxf = "";
+    for (var i=0; i<lines.length; i++) {
+        uxf += lines[i] + "\n";
+    }
+    return uxf;
+}
+
+ug.replaceXMLViewWithImage = function() {
+
+    // Use HTML that GitHub uses to display an image.
+    // e.g. On https://github.com/CalumJEadie/umlet-github/blob/master/overview.png
+    imgHTML = '<div class="blob-wrapper data type-text js-blob-data">'
+    + '<div class="image js-image">'
+    + '<span class="border-wrap">'
+    + '<img src="" alt="Loading...">'
+    + '</span>'
+    + '</div>'
+    + '</div>'
+    $(ug.FILE_VIEW).replaceWith(imgHTML)
+
+}
+
+ug.getImageURL = function(callback) {
+    uxf = ug.getUXF()
+
+    // Do Ajax Request
+    imageURL = "http://www.calumjeadie.com/"
+
+    callback(imageURL)
+}
+
+ug.updateImageURL = function(imageURL) {
+    $(ug.FILE_VIEW).find("img").attr("src", imageURL)
+}
+
+ug.replaceXMLViewWithImage()
+
+ug.getImageURL(ug.updateImageURL)
